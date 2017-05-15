303138bde279a5d71839a5c0d51769ce719f9e43
Esther DEVEZE <edeveze@student.42.fr>
2017-05-15 17:36:46 +0200

    flags added for int specifier

--- displaying.c
+++ displaying.c
@@ -13,52 +13,57 @@
 #include "ft_printf.h"
 #include <stdio.h>
 
+void finale(t_lst *list, char *str)
+{
+	char c;
+	int len;
+	int diff;
+
+	len = ft_strlen(str);
+	diff = list->width - len;
+	c = ' ';
+	if (diff > 0 && list->flag != '-')
+	{
+		if (list->flag == '0')
+				c = '0';
+		while (diff-- > 0)
+				write(1, &c,  1);
+	}
+	if ((list->flag == ' ' && list->type == ARG_STR) || (list->flag == ' ' && str[0] != '-'))
+	{
+		write(1, &c, 1);
+		list->nb = 1;
+	}
+	if (list->type == INT && list->flag == '+' && ft_atoi(str) > 0)
+	{
+		write(1, "+", 1);
+		list->nb = 1;
+	}
+	ft_putstr(str);
+	if (list->flag == '-' && diff > 0)
+	{
+		while (diff-- > len)
+			write(1, &c,  1);
+	}
+	list->nb = list->nb + (len > list->width ? ft_strlen(str)  : list->width);
+}
+
 /*
 ** Ajouter les options (largeur, flags et precision)
 */
+/* peut etre plus malin de faire une fonction avec les differents ajouts a la string initiale et ensuite de prendre le nombre de char pour le list->nb*/
 
 void	display_int(t_lst *list, va_list ap)
 {
-	int result;
-
-	result = va_arg(ap, int);
-	ft_putnbr(result);
-	list->nb = ft_countdigits(result);
-}
-
-void	display_str(t_lst *list, va_list ap)
-{
 	char *str;
-	char c;
-	int len;
-	int width;
 
-	str = ft_strdup(va_arg(ap, char*));
-	len = ft_strlen(str);
-	width = list->width;
-	if (width && list->flag != '-')
-	{
-		c = ' ';
-		if (list->flag == '0')
-				c = '0';;			
-		while (width-- > len)
-				write(1, &c,  1);
-	}
-	ft_putstr(str);
-	if (list->flag == '-' && width > len)
-	{
-		while (width-- > len)
-			write(1, " ",  1);
-
-	}
-	list->nb = (len > list->width ? ft_strlen(str)  : list->width);
+	str = (list->type == INT ? ft_itoa(va_arg(ap, int)) : ft_strdup(va_arg(ap, char*)));
+	finale(list, str);
 	free(str);
 }
 
 void	displaying(t_lst *list, va_list ap)
 {
-	if (list->type == INT)
+	if (list->type == INT || list->type == ARG_STR)
 		display_int(list, ap);
-	if (list->type == ARG_STR)
-		display_str(list, ap);
 }
