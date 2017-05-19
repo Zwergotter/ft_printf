b64360c09f78922fe533cb9fa04f9b6bcbbe9d99
Esther DEVEZE <edeveze@student.42.fr>
2017-05-19 21:04:11 +0200

    searching how to work better with options

--- displaying.c
+++ displaying.c
@@ -13,44 +13,84 @@
 #include "ft_printf.h"
 #include <stdio.h>
 
+// toute mettre dans une chaine et non pas affichage au fur et a mesure???
+// signe a une influence sur width(la longueur) mais pas sur precision
+// si precision > length -> nb zeros a ecrire = precision - length (- 1 si signe ou si flag espace existe)
+// si width > precision -> nb d'espaces a ecrire = width - (precision > length ? precision ; length)  -- signe et flag espace ne changent pas ce nombre(ne lui enleve pas un charactere, un 0, a afficher). il faut donc les rajouter en plus
+
 void finale(t_lst *list, char *str)
 {
-	char c;
+	char sign;
+	char *new;
 	int len;
-	int diff;
+	int diff_pre;
+	int diff_width;
 
-	diff = 0;
+	sign = '\0';
+	diff_pre = 0;
+	diff_width = 0;
 	len = ft_strlen(str);
+	if (list->type == INT)
+		sign = (str[0] == '-' ? '-' : '+');
+	new = (sign ? ft_strsub(str, 1, len - 1) : str);
 	if (list->type == INT && list->precision && list->precision > len)
-		diff = list->precision - len;
-	if (diff == 0)
-		diff = list->width - len;
-	c = ' ';
-	if (diff > 0 && list->flag != '-')
+		diff_pre = list->precision - len - (str[0] == '-' || list->flag == ' ' || list->flag == '+' ? 1 : 0);
+	if (list->width > list->precision)
+		diff_width = list->width - list->precision;
+	if (list->flag != '-' && diff_width > 0)
 	{
-		if (list->flag == '0')
-				c = '0';
-		while (diff-- > 0)
-				write(1, &c,  1);
+		if ((list->flag == ' ' && list->type == ARG_STR) || (list->flag == ' ' && str[0] != '-'))
+		{
+			write(1, " ", 1);
+			list->nb += 1;
+		}
+		if (list->type == INT && (list->flag == '+'|| sign == '-'))
+		{
+			write(1, &sign, 1);
+			list->nb += 1;
+			diff_width--;
+		}
+		if (diff_pre > 0)
+		{
+			while (diff_pre-- > 0)
+			{
+				write(1, "0",  1);
+				list->nb += 1;
+			}
+		}
+		ft_putstr(new);
+		while (diff_width-- > 0)
+		{
+			write(1, " ",  1);
+			list->nb += 1;
+		}
 	}
-	if ((list->flag == ' ' && list->type == ARG_STR) || (list->flag == ' ' && str[0] != '-'))
-	{
-		write(1, &c, 1);
-		list->nb = 1;
-	}
-	if (list->type == INT && list->flag == '+' && ft_atoi(str) > 0)
-	{
-		write(1, "+", 1);
-		list->nb = 1;
-	}
-	ft_putstr(str);
-	if (list->flag == '-' && diff > 0)
+	else
 	{
 		// printf("FLAG IS - : flag is %c and diff is %d\n", list->flag, diff);
-		while (diff-- > 0)
-			write(1, &c,  1);
+		while (diff_width-- > 0)
+		{
+			write(1, " ",  1);
+			list->nb += 1;
+		}
+		if (list->type == INT && sign == '-')
+		{
+			write(1, &sign, 1);
+			list->nb += 1;
+			diff_width--;
+		}
+		if (diff_pre > 0)
+		{
+			while (diff_pre-- > 0)
+			{
+				write(1, "0",  1);
+				list->nb += 1;
+			}
+		}
+		ft_putstr(new);
 	}
-	list->nb = list->nb + (len > list->width ? ft_strlen(str)  : list->width);
+	list->nb = list->nb + ft_strlen(new);
+	free(new);
 }
 
 /*
