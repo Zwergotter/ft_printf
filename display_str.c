69032005a0f28c13dc99335b8698aee21a3a1fd1
Esther DEVEZE <edeveze@student.42.fr>
2017-06-07 17:20:51 +0200

    changes made with flg 0

--- display_str.c
+++ display_str.c
@@ -6,18 +6,19 @@
 /*   By: edeveze <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/07 16:54:50 by edeveze           #+#    #+#             */
-/*   Updated: 2017/06/07 16:54:53 by edeveze          ###   ########.fr       */
+/*   Updated: 2017/06/07 17:04:03 by edeveze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 void display_str(t_lst *list, va_list ap, t_one *one)
 {
 	one->str = ft_strdup(va_arg(ap, char*));
-	one->new = (list->i_pre && list->i_pre < list->len? ft_strsub(one->str, 0, list->i_pre - 1) : one->str);
+	one->new = (list->i_pre && list->i_pre < (int)ft_strlen(one->str) ? ft_strsub(one->str, 0, list->i_pre - 1) : one->str);
 	one->len = ft_strlen(one->new);
-	one->diff_width = list->width - (list->len > list->i_pre ? list->i_pre : list->len);
+	one->diff_width = list->width - (one->len > list->i_pre ? list->i_pre : one->len);
 	if (list->pre && !list->i_pre) //si precicion existe mais qu'aucun nombre n'a ete precise
 	{
 		if (list->width)//si une largeur a ete precisee
@@ -26,7 +27,7 @@ void display_str(t_lst *list, va_list ap, t_one *one)
 	}
 	else
 	{
-		if (list->width > list->len && list->width > list->i_pre)// si largeur precisee est plus grande que longueur et precision
+		if (list->width > one->len && list->width > list->i_pre)// si largeur precisee est plus grande que longueur et precision
 		{
 			if (list->flag == '-')
 			{
