546c9ea827aac4c06e8d5ed5220416ac644886b1
Esther DEVEZE <edeveze@student.42.fr>
2017-06-13 16:44:56 +0200

    adding comments

--- displaying.c
+++ displaying.c
@@ -13,16 +13,20 @@
 #include "ft_printf.h"
 #include <stdio.h>
 
-// toute mettre dans une chaine et non pas affichage au fur et a mesure???
-// signe a une influence sur width(la longueur) mais pas sur precision
-// si precision > length -> nb zeros a ecrire = precision - length (- 1 si signe ou si flag espace existe)
-// si width > precision -> nb d'espaces a ecrire = width - (precision > length ? precision ; length)  -- signe et flag espace ne changent pas ce nombre(ne lui enleve pas un charactere, un 0, a afficher). il faut donc les rajouter en plus
+/*
+** Will write a character i times.
+*/
 
 void write_char(char c, int i)
 {
 	while (i-- > 0)
 		write(1, &c, 1);
 }
+
+/*
+** Function that will call the appropriate one in order to format and then
+** display arguments one by one and according to their types
+*/
 
 void	displaying(t_lst *list, va_list ap)
 {
